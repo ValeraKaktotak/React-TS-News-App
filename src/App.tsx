1aee94aea23057821c301cec4ff69262d62d0a7a
Valera <valerakaktotak@gmail.com>
2024-09-14 19:55:01 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='app bg-tertiary h-screen w-full p-6 transition-colors duration-500 ease-in-out'>
+			<div className='flex flex-col items-center h-full justify-between'>
+				<h1 className='text-card-foreground'>Hello World</h1>
+			</div>
+		</div>
+	)
+}
+
+export default App
