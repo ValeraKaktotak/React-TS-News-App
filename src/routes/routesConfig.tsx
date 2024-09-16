1401ea627869a50dac18289d13899ce9ac130488
Valera <valerakaktotak@gmail.com>
2024-09-16 19:20:02 +0300

    added Home to routes

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -2,10 +2,18 @@ import { createBrowserRouter } from 'react-router-dom'
 
 //Components
 import App from '@/App'
+import Home from '@/pages/Home'
 
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-	},
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      }
+    ]
+  }
 ])
+//do home page
